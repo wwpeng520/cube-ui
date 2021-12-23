eaf77f48a50d6e140cf0518b7511b6e847a1bac3
wwpeng520 <wwpeng520@gmail.com>
2021-12-23 15:46:46 +0800

    💥 feat: gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   favicon: 'https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/gold.981a5510.svg',
   logo: 'https://lf-cdn-tos.bytescm.com/obj/static/xitu_extension/static/gold.981a5510.svg',
   outputPath: 'docs-dist',
+  base: '/',
+  publicPath: 'https://wwpeng520.github.io/cube-ui/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   extraBabelPlugins: [
     [
       'import',
